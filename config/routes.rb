9a710cb60b87fcbc04fdcf2045d470a1eb2cf365
@RyoBamm <ryooooozrq1205@gmail.com>
2018-09-18 20:46:02 +0900

    Implement Incremental Search

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resources :users, only: [:index, :edit, :update]
+  resources :users, only: [:edit, :update]
+  get 'groups/users' => 'users#index'
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
+    resources :users, only: [:index]
   end
 end
